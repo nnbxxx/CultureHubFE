f9d3d1744080d7934afeb85f356bde6466fa5c28
nnbxxx <uyenbao4a5@gmail.com>
2023-05-22 20:54:31 +0700

    fix gg map library

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-import "mapbox-gl/dist/mapbox-gl.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
